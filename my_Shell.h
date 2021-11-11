596d7c128299b25dfcc531f106a20d640865b228
LiliaAzatyan <88790616+LiliaAzatyan@users.noreply.github.com>
2021-11-11 21:12:46 +0400

    Create my_Shell.h

---
+++ my_Shell.h
@@ -0,0 +1,16 @@
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+  
+#define MAXCOM 1000 
+#define MAXLIST 100 
+  
+#define clear() printf("\033[H\033[J")
+  
